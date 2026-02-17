42fa0b0ecdf2bd4bcbb8893d2ae7ba072d0aa648
Marie-Ehnert <Marie.Ehnert@jambit.com>
2026-02-17 17:59:38 +0100

    add basic update functionality

--- src/log.py
+++ src/log.py
@@ -3,9 +3,7 @@ from datetime import date
 from io import TextIOWrapper
 from pathlib import Path
 
-
 PATH = Path(__file__).parent.parent.joinpath("logs")
-print(PATH)
 
 def start_log():
     print("Welcome to game-log! \n")
@@ -22,7 +20,6 @@ def create_game_log():
     new_game = input()
     try:
         game_file = open(f"{PATH}/{new_game.replace(" ", "_")}_log.txt", "x")
-        print(game_file)
         query_player_input(game_file)
     except FileExistsError:
         print("This game is already logged, you can only update it!")
@@ -36,18 +33,24 @@ def query_player_input(game_file: TextIOWrapper):
     print("What are the next steps and goals?: ")
     future = input()
     game_file.write(future + os.linesep)
-    game_file.write(">< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< >< ><")
+    game_file.write("\n")
 
 
 def update_game_log():
     print("What game shall be logged?: ")
     game = input()
     dir_list = os.listdir(PATH)
+    game_found = False
+    game_log_path = f"{PATH}/{game.replace(" ", "_")}_log.txt"
+    game_log_file_name = f"{game.replace(" ", "_")}_log.txt"
     for file in dir_list:
-        if f"PATH{game.replace(" ", "_")}_log.txt" == file:
+        if game_log_file_name == file:
+            game_found = True
             continue
-    with open(f"{PATH}{game.replace(" ", "_")}_log.txt", "a") as file:
-        file.write("test")
+    if game_found:
+        with open(game_log_path, "a") as game_file:
+            query_player_input(game_file)
+    else: print(f"No game file found with the name {game_log_path}, did you mean to create one? If yes please restart this program and use the \"new\" command")
 
 
 
