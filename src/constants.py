65891824293db48729951c2d04677de9e20a5389
Marie-Ehnert <Marie.Ehnert@jambit.com>
2026-03-04 14:46:47 +0100

    handle not exisitng config.toml for Github actions

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,15 @@ LOG_PATH: Path = Path(__file__).parent.parent.joinpath("logs")
 CONFIG_PATH: Path = Path(__file__).parent.parent.joinpath("config.toml")
 GAMES_DATA_JSON_PATH: Path = STEAM_GAME_DATA_PATH.joinpath("games_data.json")
 
-with open(CONFIG_PATH, 'rb') as f:
-    config = tomllib.load(f)
+def load_config():
+    if not CONFIG_PATH.exists():
+        # Return default/mock values if file is missing (for CI in GitHub)
+        return {"steam_api_key": "MOCK", "steam_id_64": "0", "use_steam_stats": "False"}
+
+    with open(CONFIG_PATH, 'rb') as f:
+        return tomllib.load(f)
+
+config = load_config()
 
 API_KEY = config["steam_api_key"]
 STEAM_ID = config["steam_id_64"]
